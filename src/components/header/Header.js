e91747aae93bdde2d9259962f90ab3da18046096
Alina <alinarybak88@gmail.com>
2023-07-21 09:56:30 +0200

    changed header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -8,9 +8,9 @@ function Header(){
         <div className="logo">GreenFoodBrands</div>
         <div className="logo contact-info">Call us: (123)123-123-23, (123)123-123-24</div>
         <div class="row">
-      <a class="social-icon float-left social-icon-facebook" href="https://www.facebook.com/" aria-label="Виконати дію"></a>
-      <a class="social-icon float-left social-icon-linkedin" href="https://www.linkedin.com/" aria-label="Виконати дію"></a>
-      <a class="social-icon float-left social-icon-skype" href="https://www.skype.com/"aria-label="Виконати дію"></a>
+      <a class="social-icon float-left social-icon-facebook text-white" href="https://www.facebook.com/" aria-label="Виконати дію">t</a>
+      <a class="social-icon float-left social-icon-linkedin text-white" href="https://www.linkedin.com/" aria-label="Виконати дію">t</a>
+      <a class="social-icon float-left social-icon-skype text-white" href="https://www.skype.com/"aria-label="Виконати дію">t</a>
     </div>
       </div>
       <div className='bg-banner'></div>
